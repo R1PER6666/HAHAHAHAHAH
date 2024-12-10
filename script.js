ea11ef1fe3b85190d068d94d10a7fb7f0fe2fded
R1PER6666 <rperuntilov@gmail.com>
2024-12-10 18:05:27 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function showScare() {
+    const overlay = document.getElementById('overlay');
+    const scream = document.getElementById('scream');
+    overlay.style.display = 'flex';
+    scream.play();
+}
