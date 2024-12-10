9f1f399b0b409e58fd0a5ef980404b88457e554f
R1PER6666 <rperuntilov@gmail.com>
2024-12-10 18:45:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ function showScare() {
     const overlay = document.getElementById('overlay');
     const scream = document.getElementById('scream');
     overlay.style.display = 'flex';
+    overlay.style.justifyContent = 'center';
+    overlay.style.alignItems = 'center';
     scream.play();
 }
